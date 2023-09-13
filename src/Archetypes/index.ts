ed03ee08253756040dd3831a924364f210a6e3ce
Fabricio Floriano <f.florian0@hotmail.com>
2023-09-13 14:01:05 -0300

    Requisito 05?

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,3 +1,13 @@
 import Archetype from './Archetypes';
+import Mage from './Mage';
+import Necromancer from './Necromancer';
+import Ranger from './Ranger';
+import Warrior from './Warrior';
 
 export default Archetype;
+export {
+  Mage,
+  Necromancer,
+  Ranger,
+  Warrior,
+};
